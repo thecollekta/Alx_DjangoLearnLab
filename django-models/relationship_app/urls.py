2142474518d22f80c44e27c8aa520b8fc625c3ef
Festus Aboagye <163061224+thecollekta@users.noreply.github.com>
2024-08-16 22:35:44 +0000

    Delete django-models/relationship_app directory

--- django-models/relationship_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import book_list, LibraryDetailView
-
-urlpatterns = [
-    path('books/', book_list, name='book-list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-]
