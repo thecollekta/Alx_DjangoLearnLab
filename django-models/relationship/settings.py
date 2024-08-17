37d2e848103274b41dd77871bfc818c92d4a5514
Collekta <festus233@gmail.com>
2024-08-17 07:09:01 +0000

    Modified my settings.py file to include relationship template directory

--- django-models/relationship/settings.py
+++ django-models/relationship/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'relationship_app',
+    'relationship_app.apps.RelationshipAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'relationship.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'relationship_app/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
