bbdfd7fc0fff62e44691ad873f05798a583b036c
Collekta <festus233@gmail.com>
2024-08-17 00:15:48 +0000

    Implemented advanced model relationship in relationship_app

---
+++ relationship_app/relationship_app/query_samples.py
@@ -0,0 +1,31 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+# Query all books by a specific author.
+def books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return None
+
+
+# List all books in a library.
+def books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        return books
+    except Library.DoesNotExist:
+        return None
+
+
+# Retrieve the librarian for a library.
+def librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = Librarian.objects.get(library=library)
+        return librarian
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
