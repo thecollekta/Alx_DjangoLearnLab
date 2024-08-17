bb51b2f4379118a3aae107eb35cffe94d8ac9380
Collekta <festus233@gmail.com>
2024-08-17 20:00:09 +0000

    Implemented user authentication features (login, logout, register) in relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import CustomLogoutView
+from .views import register
 
 urlpatterns = [
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    # path('logout/', LogoutView.as_view(template_name='relationship_app/simple_logout.html'), name='logout'),
+    # path('logout/', CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
