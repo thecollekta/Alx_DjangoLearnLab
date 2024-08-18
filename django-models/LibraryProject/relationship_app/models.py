eb050631b84275f4d1cfa17965c5edfff74659d4
Collekta <festus233@gmail.com>
2024-08-18 14:57:10 +0000

    Implemented custom permissions in relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -7,11 +7,20 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 # Book Model definition
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
     def __str__(self):
         return self.title
@@ -33,7 +42,7 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -42,20 +51,22 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
+        ("Admin", "Admin"),
+        ("Librarian", "Librarian"),
+        ("Member", "Member"),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=50, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return f'{self.user.username} - {self.role}'
-    
+        return f"{self.user.username} - {self.role}"
+
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,11 +3,14 @@ from .views import admin_view, librarian_view, member_view
 from . import views
 from .views import list_books
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import CustomLogoutView
+# from .views import CustomLogoutView
 from .views import register
 
 
 urlpatterns = [
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
