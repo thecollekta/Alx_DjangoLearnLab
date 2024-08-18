a54fc6db3a59ce9a2f0f85cc60c1e9c2fe13377a
Collekta <festus233@gmail.com>
2024-08-18 00:02:32 +0000

    Implemented role-based access control to restrict access

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'relationship_app' / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
+from .views import admin_view, librarian_view, member_view
 from . import views
 from .views import list_books
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import CustomLogoutView
 from .views import register
 
+
 urlpatterns = [
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     # path('logout/', LogoutView.as_view(template_name='relationship_app/simple_logout.html'), name='logout'),
