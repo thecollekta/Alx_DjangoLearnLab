65eac492a7a1186cbb502d92c4cab4c2309bc7cb
Collekta <festus233@gmail.com>
2024-09-01 11:29:44 +0000

    Built my first API Endpoit with Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from .views import BookList
 
+
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('books/', BookList.as_view(), name='book-list'),
 ]
