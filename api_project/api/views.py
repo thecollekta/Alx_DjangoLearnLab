98ed2a95b54c33b910138fcaf23823dc9f0aed63
Collekta <festus233@gmail.com>
2024-09-01 11:22:38 +0000

    Built my first API Endpoit with Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics.ListAPIView import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
