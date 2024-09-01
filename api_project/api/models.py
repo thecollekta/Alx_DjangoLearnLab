e4c8de8d5fa99cbc0b059ef76fcad7bf55ccfa39
Collekta <festus233@gmail.com>
2024-09-01 11:18:10 +0000

    Bbuilt my first API Endpoit with Django REST Framework

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Book(models.Model):
     """Class representing a book"""
 
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
