21c6bec8122e08357e4650ddf857ece44ecf9b5a
Collekta <festus233@gmail.com>
2024-09-08 23:51:03 +0000

    Wrote Unit Tests for Django REST Framework APIs

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -7,7 +7,6 @@ from rest_framework import status
 from .models import Book, Author
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from rest_framework.test import APITestCase
 
 User = get_user_model()
 
