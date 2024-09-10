d332d85c7b0e87f77ae35d7cf8c95a389681f616
Collekta <festus233@gmail.com>
2024-09-10 14:02:13 +0000

    Implementing the Blog's User Authentication System

--- django_blog/static/js/script.js
+++
@@ -1,5 +0,0 @@
-// blog/static/js/script.js
-// Basic example script to demonstrate dynamic behavior
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Blog page loaded');
-});
