538a52614eaa24f016092326c81cf189ebb44ce0
Collekta <festus233@gmail.com>
2024-09-10 18:08:07 +0000

    Creating Blog Post Management Features

--- django_blog/staticfiles/js/scripts.js
+++
@@ -1,5 +0,0 @@
-// blog/static/js/scripts.js
-// Basic example script to demonstrate dynamic behavior
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Blog page loaded');
-});
