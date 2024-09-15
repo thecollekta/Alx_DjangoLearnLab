186f6f8213c00d9f990fa231bf2213369fcbcdef
Collekta <festus233@gmail.com>
2024-09-15 17:07:39 +0000

    Refactored code

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,11 +2,18 @@
 
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from blog import views as user_login
 from .views import (homepage, PostListView, PostDetailView, PostCreateView, 
                     PostUpdateView, PostDeleteView, CommentCreateView, 
                     CommentUpdateView, CommentDeleteView, search_posts)
 
-urlpatterns = [ 
+urlpatterns = [
+    # User authentication
+    path('register/', user_login.register, name='register'),
+    path('profile/', user_login.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'), 
     # Post list/detail view and CRUD operations for posts
     path('', homepage, name='home'), # Homepage URL
     path('posts/', PostListView.as_view(), name='posts'), # Blog Posts list
