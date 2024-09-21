c42fffe0c1f7f8269586c58551209951aa994107
Collekta <festus233@gmail.com>
2024-09-21 23:06:26 +0000

    Modified the posts/urls.py file

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -11,4 +11,8 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='feed'),
+    path('posts/<int:pk>/like/', PostViewSet.as_view({'post': 'like'}), 
+         name='post-like'),
+    path('posts/<int:pk>/unlike/', PostViewSet.as_view({'post': 'unlike'}), 
+         name='post-unlike'),
 ]
